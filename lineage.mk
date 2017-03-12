Myself5/android_device_sony_sumire-1

sumire: update Fingerprint, add network props
Change-Id: I96fec914eb450c95f682fdde05e68a258ce0a58d

lineage.mk
@@ -25,5 +25,5 @@ PRODUCT_NAME := lineage_sumire
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sumire \
-    BUILD_FINGERPRINT=Sony/sumire/sumire:6.0.1/KITAKAMI-2.1.2-SEC-161031-1748/1:user/dev-keys \
-    PRIVATE_BUILD_DESC="sumire-user 6.0.1 KITAKAMI-2.1.2-SEC-161031-1748 1 dev-keys"
+    BUILD_FINGERPRINT=Sony/sumire/sumire:7.0/KITAKAMI-2.2.0-170123-1244/1:user/dev-keys \
+    PRIVATE_BUILD_DESC="sumire-user 7.0 KITAKAMI-2.2.0-170123-1244 1 dev-keys"

shunoDroid/android_device_sony_satsuki

lineage.mk
@@ -26,5 +26,5 @@ PRODUCT_NAME := lineage_satsuki
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=satsuki \
-    BUILD_FINGERPRINT=Sony/satsuki/satsuki:6.0.1/KITAKAMI-2.1.2-SEC-161031-1748/1:user/dev-keys \
-    PRIVATE_BUILD_DESC="satsuki-user 6.0.1 KITAKAMI-2.1.2-SEC-161031-1748 1 dev-keys"
+    BUILD_FINGERPRINT=Sony/satsuki/satsuki:7.0/KITAKAMI-2.2.0-170123-1244/1:user/dev-keys \
+    PRIVATE_BUILD_DESC="satsuki-user 7.0 KITAKAMI-2.2.0-170123-1244 1 dev-keys"